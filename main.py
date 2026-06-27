7d459be73ba6bc965df584e60a8f403285786639
Nishanth Prabhu <beanzisepic@gmail.com>
2026-06-26 19:38:29 -0700

    Added feature that highlights the corresponding number to the needle

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ scale = 1.6
 needle_img = padded.resize((int(292 * scale), int(260 * scale)), Image.NEAREST)
 needle_frames = []
 shadow_frames = []
-for i in range(1080):
-    angle = i * (360 / 1080)
+for i in range(500):
+    angle = i * (360 / 500)
     rotated = needle_img.rotate(-angle, resample=Image.BICUBIC, expand=False)
     needle_frames.append(ImageTk.PhotoImage(rotated))
     r, g, b, a= rotated.split()
@@ -70,7 +70,7 @@ needle_angle = 0
 needledir = 1
 def rotate_needle():
     global needle_angle
-    needle_angle = (needle_angle + 8 * needledir) % 1080    #here change speed
+    needle_angle = (needle_angle + 6 * needledir) % 500    #here change speed
     canvas.itemconfig(shadow, image=shadow_frames[needle_angle])
     canvas._shadow = shadow_frames[needle_angle]
     canvas.itemconfig(needle, image=needle_frames[needle_angle])
@@ -81,35 +81,47 @@ def flipdirection(e=None):
     needledir *= -1
 canvas.bind("<Button-1>", flipdirection)
 canvas.bind("<space>", flipdirection)
+lasthigh = [None]
+numhigh = {}
+def highlightnumb():
+    degrees = (needle_angle / 500) * 360
+    number = int((degrees / 30+ 3.3) % 12) or 12
+    if lasthigh[0] != number:
+        if lasthigh[0] in numhigh:
+            canvas.itemconfig(numhigh[lasthigh[0]], fill="white")
+        if number in numhigh:
+            canvas.itemconfig(numhigh[number], fill="#353232")
+        lasthigh[0] = number
+    app.after(10, highlightnumb)
 def normal(canvas, canvas_img):
     global needle, shadow
     clear(canvas, canvas_img)
     canvas.create_text(353, 23, text='POP THE CLOCK!', font=("Press Start 2P", 22), fill="#968d8d", anchor='center')
     canvas.create_text(350, 20, text="POP THE CLOCK!", font=("Press Start 2P", 22), fill="#ffffff", anchor='center')
-    canvas.create_text(351, 143, text='12', font=("Press Start 2P", 20), fill="#968d8d", anchor='center')
-    canvas.create_text(348, 140, text="12", font=("Press Start 2P", 20), fill="white", anchor='center')
-    canvas.create_text(453, 184, text='1', font=("Press Start 2P", 20), fill="#968d8d", anchor='center')
-    canvas.create_text(450, 181, text='1', font=("Press Start 2P", 20), fill='white', anchor='center')
-    canvas.create_text(517, 263, text='2', font=("Press Start 2P", 20), fill='#968d8d', anchor='center')
-    canvas.create_text(514, 260, text='2', font=("Press Start 2P", 20), fill='white', anchor='center')
-    canvas.create_text(541, 353, text='3', font=("Press Start 2P",20), fill="#967d8d", anchor='center')
-    canvas.create_text(538, 350, text='3', font=("Press Start 2P", 20), fill="white", anchor='center')
-    canvas.create_text(517, 443, text='4', font=("Press Start 2P", 20), fill="#967d8d", anchor='center')
-    canvas.create_text(514, 440, text='4', font=("Press Start 2P", 20), fill="white", anchor='center')
-    canvas.create_text(453, 516, text='5', font=("Press Start 2P", 20), fill="#967d8d", anchor='center')
-    canvas.create_text(450, 513, text='5', font=("Press Start 2P", 20), fill='white', anchor='center') 
-    canvas.create_text(353, 553, text='6', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
-    canvas.create_text(350, 550, text="6", font=("Press Start 2P", 20), fill="white", anchor='center')
-    canvas.create_text(253, 516, text='7', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
-    canvas.create_text(250, 513, text='7', font=('Press Start 2P', 20), fill="white", anchor='center')
-    canvas.create_text(167, 443, text='8', font=('Press Start 2P', 20), fill='#967d8d', anchor='center')
-    canvas.create_text(164, 440, text='8', font=("Press Start 2P", 20), fill='white', anchor='center')
-    canvas.create_text(155, 353, text='9', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
-    canvas.create_text(152, 350, text='9', font=("Press Start 2P", 20), fill='white', anchor='center')
-    canvas.create_text(179, 263, text='10', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
-    canvas.create_text(176, 260, text='10', font=("Press Start 2P", 20), fill='white', anchor='center')
-    canvas.create_text(250, 184, text='11', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
-    canvas.create_text(247, 181,text='11', font=("Press Start 2P", 20), fill='white', anchor='center' )
+    numhigh[12] = canvas.create_text(351, 143, text='12', font=("Press Start 2P", 20), fill="#968d8d", anchor='center')
+    numhigh[12] = canvas.create_text(348, 140, text="12", font=("Press Start 2P", 20), fill="white", anchor='center')
+    numhigh[1] = canvas.create_text(453, 184, text='1', font=("Press Start 2P", 20), fill="#968d8d", anchor='center')
+    numhigh[1] = canvas.create_text(450, 181, text='1', font=("Press Start 2P", 20), fill='white', anchor='center')
+    numhigh[2] = canvas.create_text(517, 263, text='2', font=("Press Start 2P", 20), fill='#968d8d', anchor='center')
+    numhigh[2] = canvas.create_text(514, 260, text='2', font=("Press Start 2P", 20), fill='white', anchor='center')
+    numhigh[3] = canvas.create_text(541, 353, text='3', font=("Press Start 2P",20), fill="#967d8d", anchor='center')
+    numhigh[3]= canvas.create_text(538, 350, text='3', font=("Press Start 2P", 20), fill="white", anchor='center')
+    numhigh[4] = canvas.create_text(517, 443, text='4', font=("Press Start 2P", 20), fill="#967d8d", anchor='center')
+    numhigh[4] = canvas.create_text(514, 440, text='4', font=("Press Start 2P", 20), fill="white", anchor='center')
+    numhigh[5] = canvas.create_text(453, 516, text='5', font=("Press Start 2P", 20), fill="#967d8d", anchor='center')
+    numhigh[5]= canvas.create_text(450, 513, text='5', font=("Press Start 2P", 20), fill='white', anchor='center') 
+    numhigh[6] = canvas.create_text(353, 553, text='6', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
+    numhigh[6] = canvas.create_text(350, 550, text="6", font=("Press Start 2P", 20), fill="white", anchor='center')
+    numhigh[7] = canvas.create_text(253, 516, text='7', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
+    numhigh[7]= canvas.create_text(250, 513, text='7', font=('Press Start 2P', 20), fill="white", anchor='center')
+    numhigh[8] = canvas.create_text(167, 443, text='8', font=('Press Start 2P', 20), fill='#967d8d', anchor='center')
+    numhigh[8] = canvas.create_text(164, 440, text='8', font=("Press Start 2P", 20), fill='white', anchor='center')
+    numhigh[9] = canvas.create_text(155, 353, text='9', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
+    numhigh[9] = canvas.create_text(152, 350, text='9', font=("Press Start 2P", 20), fill='white', anchor='center')
+    numhigh[10] = canvas.create_text(179, 263, text='10', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
+    numhigh[10] = canvas.create_text(176, 260, text='10', font=("Press Start 2P", 20), fill='white', anchor='center')
+    numhigh[11] = canvas.create_text(250, 184, text='11', font=("Press Start 2P", 20), fill='#967d8d', anchor='center')
+    numhigh[11] = canvas.create_text(247, 181,text='11', font=("Press Start 2P", 20), fill='white', anchor='center' )
     canvas._needle = needle_frames[0]
     shadow = canvas.create_image(354, 354, anchor='center', image=needle_frames[0])
     canvas._shadow = shadow_frames[0]
@@ -118,7 +130,7 @@ def normal(canvas, canvas_img):
     canvas.lift(needle)
 normal(canvas, canvasbg)
 rotate_needle()
-
+highlightnumb()
 
 
 
